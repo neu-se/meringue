ab5891c57291c97cf0e781377595f3c0b2bee17f
Tony Wang <tony.wanghao@stu.pku.edu.cn>
2022-09-06 19:31:38 +0800

    attempt to add support for JUnitParamsRunner

--- meringue-zest-extension/src/main/java/edu/berkeley/cs/jqf/ZestForkMain.java
+++ meringue-zest-extension/src/main/java/edu/berkeley/cs/jqf/ZestForkMain.java
@@ -8,6 +8,21 @@ import org.junit.runner.RunWith;
 import org.junit.runner.Runner;
 import org.junit.runners.Parameterized;
 import org.junit.runners.model.MultipleFailureException;
+import junitparams.JUnitParamsRunner;
+
+import javassist.NotFoundException;
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.CtMethod;
+import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.ConstPool;
+import javassist.bytecode.MethodInfo;
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.annotation.Annotation;
+import javassist.bytecode.annotation.BooleanMemberValue;
+import javassist.bytecode.annotation.ClassMemberValue;
 
 import java.io.File;
 
@@ -33,6 +48,17 @@ public final class ZestForkMain {
             GuidedFuzzing.run(testClass, testMethodName, guidance, System.out);
         } else if (runnerClass.equals(Parameterized.class)) {
             ParameterizedZestRunner.run(testClass, testMethodName, guidance);
+        } else if (runnerClass.equals(JUnitParamsRunner.class)) {
+            try {
+                //testClass = addJQFAnnotationToClass(testClass);
+                addJQFAnnotationToClass(testClass);
+            } catch (Exception e) {
+                System.out.println(e.toString());
+                throw new IllegalArgumentException("Unknown test class type:" + testClass);
+            }
+            RunWith annotation = testClass.getAnnotation(RunWith.class);
+            System.out.println(annotation.value().getSimpleName());
+            GuidedFuzzing.run(testClass, testMethodName, guidance, System.out);
         } else {
             throw new IllegalArgumentException("Unknown test class runner type:" + runnerClass);
         }
@@ -44,5 +70,24 @@ public final class ZestForkMain {
         }
         RunWith annotation = testClass.getAnnotation(RunWith.class);
         return annotation.value();
+    }
+    
+    private static void addJQFAnnotationToClass (Class<?> testClass) throws NotFoundException,CannotCompileException {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.makeClass(testClass.getName());
+
+        ClassFile classFile = ctClass.getClassFile();
+        ConstPool constpool = classFile.getConstPool();
+
+        AnnotationsAttribute annotationsAttribute = new AnnotationsAttribute(constpool, AnnotationsAttribute.visibleTag);
+        Annotation annotation = new Annotation(RunWith.class.getName(), constpool);
+        annotation.addMemberValue("value()", new ClassMemberValue(JQF.class.getName(), classFile.getConstPool()));
+        annotationsAttribute.setAnnotation(annotation);
+
+        ctClass.getClassFile().addAttribute(annotationsAttribute);
+        //Class<?> ret = ctClass.toClass();
+        //System.out.println("GOODDDD");
+
+        //return ret;
     }
 }
