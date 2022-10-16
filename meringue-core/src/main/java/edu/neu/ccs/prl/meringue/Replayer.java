9b3c828a591325571b2719eb0de436ed3b9a6b08
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2022-10-16 14:06:26 -0400

    * restructured Replayer interface

--- meringue-core/src/main/java/edu/neu/ccs/prl/meringue/Replayer.java
+++ meringue-core/src/main/java/edu/neu/ccs/prl/meringue/Replayer.java
@@ -1,10 +1,11 @@
 package edu.neu.ccs.prl.meringue;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface Replayer {
     void configure(String testClassName, String testMethodName, ClassLoader classLoader)
-            throws ReflectiveOperationException;
+            throws ReflectiveOperationException, IOException;
 
     Throwable execute(byte[] input, File inputFile) throws Throwable;
 }
